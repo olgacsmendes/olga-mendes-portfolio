89f701bb35a56b03ab9836ab06ae622833c0d04a
Olga Mendes <olgacsmendes@gmail.com>
2024-01-08 18:55:51 -0300

    fix: responsive navbar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-    const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.sub-icon');
+    const menu = document.querySelector('#sub-nav .menu-links');
+    const icon = document.querySelector('#sub-nav .sub-icon');
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
